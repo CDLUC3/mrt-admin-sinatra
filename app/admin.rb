e8a55b57b85768db84c0ba546bf32bb00675f072
Terry Brady <terrywbrady@gmail.com>
2024-11-27 11:53:53 -0800

    list all repos

--- app/admin.rb
+++ app/admin.rb
@@ -7,7 +7,7 @@ set :bind, '0.0.0.0'
 get "/" do
   status 200
 
-  erb :index
+  erb :index, :locals => {repos: %w[mrt-admin-lambda mrt-dashboard mrt-store mrt-ingest mrt-inventory mrt-audit mrt-replic]}
 end
 
 get "/git/*" do |repo|
