61381fd31db790078f8d24d59647441f2f9e9ac9
Terry Brady <terrywbrady@gmail.com>
2024-12-17 09:23:26 -0800

    dump env stuff

--- app/admin.rb
+++ app/admin.rb
@@ -6,6 +6,9 @@ require_relative 'lib/routes/code'
 
 set :bind, '0.0.0.0'
 
+puts "ENV: #{ENV}"
+puts "headers: #{headers}"
+
 # sinatra routes
 module Sinatra
   # modularize route handling by specific clients
