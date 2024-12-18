c5bed916ea03515e11d5acb3da5aabb8a968ddb3
Terry Brady <terrywbrady@gmail.com>
2024-12-17 16:18:39 -0800

    debug

--- lambda.rb
+++ lambda.rb
@@ -19,7 +19,7 @@ def handler(event:, context:)
     ENV['LAMBDA_CONTEXT_FUNCTION_ARN']=context.invoked_function_arn
     ENV['LAMBDA_CONTEXT_MEMORY_LIMIT_IN_MB']=context.memory_limit_in_mb.to_s
     ENV['LAMBDA_CONTEXT_TIMEOUT_MS']=context.methods.to_s
-  resuce StandardError => e
+  rescue StandardError => e
   end
 
   # Check if the body is base64 encoded. If it is, try to decode it
