30ae2a3293fb6d0d5552c717b4d79ec5af38643e
Terry Brady <terrywbrady@gmail.com>
2024-12-19 12:45:39 -0800

    entrypoint

--- lambda.rb
+++ lambda.rb
@@ -12,12 +12,13 @@ $app ||= Rack::Builder.parse_file("#{__dir__}/app/config.ru")
 ENV['RACK_ENV'] ||= 'production'
 
 def handler(event:, context:)
-  ENV['LAMBDA_CONTEXT_FUNCTION_NAME']=context.function_name
-  ENV['LAMBDA_CONTEXT_FUNCTION_VERSION']=context.function_version
-  ENV['LAMBDA_CONTEXT_FUNCTION_VERSION']=context.function_version
-  ENV['LAMBDA_CONTEXT_FUNCTION_ARN']=context.invoked_function_arn
-  ENV['LAMBDA_CONTEXT_MEMORY_LIMIT_IN_MB']=context.memory_limit_in_mb.to_s
-  ENV['LAMBDA_CONTEXT_TIMEOUT_MS']=context.get_remaining_time_in_millis.to_s
+  unless context.nil?
+    ENV['LAMBDA_CONTEXT_FUNCTION_NAME']=context.function_name
+    ENV['LAMBDA_CONTEXT_FUNCTION_VERSION']=context.function_version
+    ENV['LAMBDA_CONTEXT_FUNCTION_ARN']=context.invoked_function_arn
+    ENV['LAMBDA_CONTEXT_MEMORY_LIMIT_IN_MB']=context.memory_limit_in_mb.to_s
+    ENV['LAMBDA_CONTEXT_TIMEOUT_MS']=context.get_remaining_time_in_millis.to_s
+  end
 
   # Check if the body is base64 encoded. If it is, try to decode it
   body =
