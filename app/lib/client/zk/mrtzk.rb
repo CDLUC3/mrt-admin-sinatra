fea0e415e4030f57df190988143182db3a4df001
Terry Brady <terrence.brady@ucop.edu>
2025-01-23 10:21:24 -0800

    change status check

--- app/lib/client/zk/mrtzk.rb
+++ app/lib/client/zk/mrtzk.rb
@@ -6,7 +6,8 @@ require_relative '../../ui/context'
 
 # Scope custom code for UC3 to distinguish from 3rd party classes
 module UC3Queue
-  # Query for repository images by tag
+  # Query for ZK nodes
+  # VPC peering does not allow this, so this connection will not work until ZK is running in the UC3
   class ZKClient < UC3::UC3Client
     def self.client
       UC3::UC3Client.clients.fetch(self.class.to_s, ZKClient.new)
@@ -16,7 +17,7 @@ module UC3Queue
       puts MerrittZK::JobState::Estimating
       map = UC3::UC3Client.lookup_map('app/config/mrt/zk.yml')
       @zk = ZK.new(map.fetch('zkconn', ''), timeout: 1)
-      @zk.children('/')
+      @zk.exists('/')
       super(enabled: true)
     rescue StandardError => e
       super(enabled: false, message: e.to_s)
