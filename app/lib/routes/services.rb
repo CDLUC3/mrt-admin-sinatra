134642721128101a86972d5e13225a3a32e5fd0e
Terry Brady <terrywbrady@gmail.com>
2025-09-15 21:29:55 -0700

    rubocop fix

--- app/lib/routes/services.rb
+++ app/lib/routes/services.rb
@@ -305,7 +305,7 @@ module Sinatra
     end
 
     def post_url_body(url, body: nil, user: nil, password: nil)
-      puts "URI: #{url}, body: #{body}, user: #{user}, password: #{password ? '****' : nil}"
+      puts "URI: #{url}, body: #{body}, user: #{user}, password: #{'****' if password}"
       uri = URI.parse(url)
       req = Net::HTTP::Post.new(uri)
       if user && password
