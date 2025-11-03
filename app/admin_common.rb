25a8f728754448c0733c8d62fa94d483e9e5e10f
Terry Brady <terrywbrady@gmail.com>
2025-11-03 10:57:42 -0800

    csv generate utf8

--- app/admin_common.rb
+++ app/admin_common.rb
@@ -18,9 +18,9 @@ def adminui_show_table_format(context, table, format, erb: :table, locals: {})
     }.to_json
   when 'csv'
     fname = "mrt-admin#{context.route.gsub('/', '-')}.#{Time.now.strftime('%Y%m%d-%H%M%S')}.csv"
-    content_type :text, encoding: 'utf-8'
-    halt 200, { 'Content-Type' => 'text/csv; charset=utf-8', 'Content-Disposition' => "attachment; filename=\"#{fname}\"" },
-      table.to_csv
+    content_type 'text/csv', encoding: 'utf-8'
+    disposition "attachment; filename=\"#{fname}\""
+    halt 200, table.to_csv
   when 'text'
     content_type :text, encoding: 'utf-8'
     halt 200, table.to_csv
