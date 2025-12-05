44ff552699834b636b205e25544d465bd1cc5f36
Terry Brady <terrywbrady@gmail.com>
2025-12-05 15:09:56 -0800

    add menu item

--- app/lib/routes/metrics.rb
+++ app/lib/routes/metrics.rb
@@ -8,7 +8,7 @@ module Sinatra
   # client specific routes
   module UC3MetricsRoutes
     def self.registered(app)
-      app.get '/ops/metrics' do
+      app.get '/ops/metrics/benchmark-retrieval' do
         adminui_show_table(
           AdminUI::Context.new(request.path, request.params),
           UC3CloudWatch::MetricsClient.client.metric_table(
