24875a82cb22c5780d7c8084b3df84be66d51d3b
Terry Brady <terrywbrady@gmail.com>
2026-02-27 17:20:10 -0800

    fix call to query for metrics

--- app/lib/routes/services.rb
+++ app/lib/routes/services.rb
@@ -564,7 +564,7 @@ module Sinatra
         metrics.merge!({
           number_of_active_replications: -1,
           number_of_bytes_to_be_replicated: -1,
-          oldest_audit_in_days: run_query_get_val('/queries/misc/oldest-audit-in-days')
+          oldest_audit_in_days: UC3Query::QueryClient.client.run_query_get_val('/queries/misc/oldest-audit-in-days')
         })
 
         metrics.to_json
