398adb32004cbaa5b76ef8bb7a227d163dc3fb17
Terry Brady <terrywbrady@gmail.com>
2026-08-07 17:22:22 -0700

    change default back to 50

--- app/lib/routes/services.rb
+++ app/lib/routes/services.rb
@@ -436,7 +436,7 @@ module Sinatra
         raise 'Collection purge not allowed' if UC3Query::QueryResolvers.collection_purge_disabled?
 
         urlparams = {}
-        urlparams['count'] = [250, request.params.fetch('count', '250').to_i].min
+        urlparams['count'] = [250, request.params.fetch('count', '50').to_i].min
         urlparams['mnemonic'] = mnemonic
         urlparams['days'] = request.params.fetch('days', '3').to_i
         resp = UC3Query::QueryClient.client.run_query('/queries/misc/purgable_arks', urlparams).map do |row|
